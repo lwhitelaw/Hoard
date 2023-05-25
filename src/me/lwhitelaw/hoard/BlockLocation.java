1ed6821c37a4e49d3abcefa6b8feb008f3dc2b79
lwhitelaw <48133276+lwhitelaw@users.noreply.github.com>
2023-05-24 23:06:38 -0400

    start removing old Hoard code: block location

--- src/me/lwhitelaw/hoard/BlockLocation.java
+++
@@ -1,7 +0,0 @@
-package me.lwhitelaw.hoard;
-
-/**
- * The data needed to locate and extract a block payload.
- *
- */
-record BlockLocation(byte[] hash, long blockFileLocation, int blockEncoding, short blockLength, short blockEncodedLength) {}
