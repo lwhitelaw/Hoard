aa47ce589cff069d5493f51ba5ee41e6a23e8dd3
lwhitelaw <48133276+lwhitelaw@users.noreply.github.com>
2023-06-08 22:32:41 -0400

    complete packfile merging function; not yet tested

--- src/me/lwhitelaw/hoard/util/Packfiles.java
+++ src/me/lwhitelaw/hoard/util/Packfiles.java
@@ -1,6 +1,14 @@
 package me.lwhitelaw.hoard.util;
 
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
+
+import me.lwhitelaw.hoard.PackfileEntry;
+import me.lwhitelaw.hoard.PackfileReader;
+import me.lwhitelaw.hoard.PackfileWriter;
 
 /**
  * Operations on packfiles.
@@ -16,6 +24,33 @@ public class Packfiles {
 	 * @return true if the merge succeeded
 	 */
 	public boolean mergePackfiles(Path dest, Path... srcs) {
-		return false;
+		try {
+			// Open the writer
+			PackfileWriter destWriter = new PackfileWriter(dest);
+			// Open each packfile in turn...
+			for (Path src : srcs) {
+				PackfileReader reader = new PackfileReader(src);
+				// Enumerate the blocks in this packfile
+				List<PackfileEntry> blocks = reader.enumerateBlocks();
+				// For each block...
+				for (PackfileEntry block : blocks) {
+					// Read the payload and write it to the destination
+					ByteBuffer payload = reader.readPackfileEntryPayload(block, false);
+					destWriter.writeBlock(payload);
+				}
+				// Close the current source packfile
+				reader.close();
+			}
+			// Close the destination packfile
+			destWriter.close();
+			return true;
+		} catch (IOException ex) {
+			try {
+				Files.delete(dest);
+			} catch (IOException ex2) {
+				// deliberately ignored
+			}
+			return false;
+		}
 	}
 }
